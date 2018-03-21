80849e9771fb79403042b90f8705e15decffd98d
zeeo165 <joar.eliasson149@gmail.com>
2018-03-21 18:33:24 +0100

    inital commit agian

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+/*This time you are gona get commited*/
 /*Pages*/
     let pdbtn = $("#produckter");
     let formbtn = $("#checkout");
@@ -31,7 +31,7 @@
             let button = $("<button></button>");
             let link = $("<a></a>");
 
-            $(button).attr("class", "add_to_cart");
+            $(button).attr("class", "add-to-cart");
             $(link).attr("class", "recen" + [data.Id]);
             
 
@@ -69,6 +69,117 @@
 
     console.log(data);
 }
+
+/*Products in cart*/
+            
+$( document ).ready(function() {
+    $(".add-to-cart").click(function(event){
+        event.preventDefault();
+        let id = $(this).attr("data-id");
+        let name = $(this).attr("data-name");
+        let price = Number($(this).attr("data-price"));
+        let description = ($(this).attr("data-description"));
+        let image = ($(this).attr("data-image"));
+        let url = ($(this).attr("data-url"));
+    
+        addItemToCart(id, name, price,description,image,url, 1);
+        displayCart();
+    });
+});
+
+function displayCart(){
+    let cartArray = listCart();
+    let output = " " + countCart();
+    $("#shoppingCart").html(output);  
+}
+
+$( document ).ready(function() {
+$("#clear-cart").click(function(event){
+    clearCart();
+    displayCart();
+})
+});
+
+/* Shopping carts functions */
+
+let cart = [];
+
+let Item = function(id, name, price, description,image,url, count) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.image = image;
+    this.url = url;
+    this.count = count;
+}
+
+function addItemToCart(id, name, price,description,image,url, count) {
+    for(let i in cart) {
+        if(cart[i].name === name){
+            cart[i].count += count;
+            saveCart();
+            return;
+        }
+    }
+    let item = new Item(id, name, price,description,image,url, count);
+    cart.push(item);
+    saveCart();
+}
+
+function removeItemFromCart(name) {
+    for(let i in cart){
+        if(cart[i].name === name) {
+            cart[i].count --;
+
+            if(cart[i].count === 0) {
+                cart.splice(i, 1);
+            }
+        }
+    }
+    saveCart();
+}
+
+function removeItemFromCartAll(name) {
+    for(let i in cart) {
+        if(cart[i].name === name){
+            cart.splice(i, 1);
+            break;
+        }
+    }
+    saveCart();
+}
+
+function clearCart() {
+    cart = [];
+    saveCart();
+}
+
+function countCart() {
+    let totalCount = 0;
+    for(let i in cart){
+        totalCount += cart[i].count;
+    }
+
+    return totalCount;
+}
+
+function listCart() {
+    return JSON.parse(JSON.stringify(cart));
+}
+
+function saveCart() {
+    localStorage.setItem("shoppingCart", JSON.stringify(cart));
+}
+
+
+function loadCart() {
+    cart = JSON.parse(localStorage.getItem("shoppingCart"));
+}
+
+loadCart();
+displayCart();
+
 /*CheckName*/ 
     function checkName(){
         let name = $("#Firstname").val();
@@ -218,3 +329,4 @@
         $(borderLocation).css("border-color",color);
         $(borderLocation2).css("border-color",color);
 }
+
--- js/review.js
+++ js/review.js
@@ -1,28 +1,4 @@
-/*let pVar1 = $("#nameProduckt");
-let pVar2 = $("#reviewProduckt");
-let pVar3 = $("#rateProduckt");
-
-    $(".postReview").click(function(event){
-        let div = $("#reviewsProduckt");
-        let name = $("<input></input>");
-        let space1 = $("<br>");
-        let space2 = $("<br>");
-        let review = $("<textarea></textarea>");
-        let rating = $("<p></p>");
-    
-        $("#reviewsProduckt").append(name,space1,space2,review,rating);
-    
-        $(name).attr("value", pVar1.val());
-        $(name).prop("readonly", true);
-
-        $(review).html(pVar2.val());
-        $(review).prop("readonly", true);
-
-        $(rating).html("Stars " + pVar3.val());
-    });   
-    */
-    
-    
+/*rewiws*/    
     let div = $("#review");
     
     
